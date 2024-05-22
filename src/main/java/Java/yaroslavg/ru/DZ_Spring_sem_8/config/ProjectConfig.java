e2d8976162d99f313f6454dd42acaea683982735
Yaroslav <rus070392@yandex.ru>
2024-05-22 15:38:19 +0700

    HW8

---
+++ src/main/java/Java/yaroslavg/ru/DZ_Spring_sem_8/config/ProjectConfig.java
@@ -0,0 +1,24 @@
+package Java.yaroslavg.ru.DZ_Spring_sem_8.config;
+
+import Java.yaroslavg.ru.DZ_Spring_sem_8.aspect.LoggingAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan
+@EnableAspectJAutoProxy
+
+/**
+ * Конфигурационный класс для проекта.
+ */
+public class ProjectConfig {
+    @Bean
+    /**
+     * Метод, создающий и возвращающий экземпляр LoggingAspect.
+     */
+    public LoggingAspect loggingAspect(){
+        return new LoggingAspect();
+    }
+}
